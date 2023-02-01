1d9401e2b47d14b08932333c62a5e4864b9d1800
mphoola <promisemphoola2@gmail.com>
2023-02-01 17:47:21 -0300

    desktop version done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {},
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true
+    },
+  },
   plugins: [],
 };
